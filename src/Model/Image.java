a08261c34ee3a5b7904ee29642c233e3c1036721
sandsirois <sandra.sirois.1@ulaval.ca>
2017-02-19 21:49:33 -0500

    creation de la class image - en cours

---
+++ src/Model/Image.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Model;
+
+/**
+ * @author Sandra
+ *
+ */
+public class Image {
+	
+	//Constructeur par défaut
+	public Image(){ }
+	
+	void Import(){};
+	void Export(){};
+	void ColorSpace(){};
+	void Treatment(){};
+	
+	
+	  
+	
+	
+
+}
