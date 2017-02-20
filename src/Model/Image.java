e17e58379ecbb34c5fd6b4fdec21e8005b141114
sandsirois <sandra.sirois.1@ulaval.ca>
2017-02-20 15:59:13 -0500

    Update Image.java

--- src/Model/Image.java
+++ src/Model/Image.java
@@ -7,19 +7,64 @@ package Model;
  * @author Sandra
  *
  */
+
 public class Image extends PApplet{
+	//variables privees
+	private String m_fileName;
+	private int m_weight;
+	private int m_height;
 	
 	//Constructeur par défaut
-	public Image(){ }
+	public Image(){
+		m_weight = 512;
+		m_height = 512;
+		m_fileName = "";
+	}
 	
-	void Import(){};
-	void Export(){};
-	void ColorSpace(){};
-	void Treatment(){};
+	//Constructeur avec paramètres
+ 	public Image(String pFileName, int pWeight, int pHeight, ) {
+		m_fileName = pFileName;
+		m_weight = pWeight;
+		m_height = pHeight;
+	}        
 	
+	//*************   ACCESSEURS *************
+	//Retourne le nom du fichier image
+	public String getFileName() {  
+		return m_fileName;
+	}
+
+  	//Retourne la largeur du fichier image
+	public int getWeight() {
+		return m_weight;
+	}
+   
+  	// Retourne la hauteur du fichier image
+	public int getHeight() {
+		return m_height;
+	} 
+
+	//*************   MUTATEURS   *************
 	
-	  
+	//Définit le nom du fichier image
+	public void setName(String pFileName) {
+		m_fileName = pFileName;
+	}
+
+	//Définit la largeur du fichier image
+	public void setWeight(int pWeight) {
+	    m_weight = pWeight;
+	}
+
+	//Définit la largeur du fichier image
+	public void setHeight(int pHeight) {
+	    m_height = pHeight;
+	}
 	
-	
+	//*************   METHODES   *************
+	Import(String pNom){};
+	Export(){};
+	ColorSpace(){};
+	Treatment(){};
 
 }
