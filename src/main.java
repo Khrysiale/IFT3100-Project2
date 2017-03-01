c2c0b03473c6f2d0b057a2f055cf127bad1126c9
khrysiale <coulombechristine01@gmail.com>
2017-03-01 03:06:01 -0500

    tentative interface et applet

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import Application.ApplicationController;
+
+public class main {
+
+	public static void main(String[] args) {
+		ApplicationController controller = new ApplicationController();
+		controller.pack();
+	}
+
+}
