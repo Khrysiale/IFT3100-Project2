ad632532c523555628c707935e5cf5f32b358ebc
sandsirois <sandra.sirois.1@ulaval.ca>
2017-03-13 19:56:34 -0400

    dialog

--- src/PropertyWindow.java
+++ src/PropertyWindow.java
@@ -1,8 +1,13 @@
-import javafx.scene.control.Dialog;
+import java.awt.Dialog;
 
 //FenetreProprietes
 /*fenetre qui presente les proprietes d’une figure*/
 
 public class PropertyWindow extends Dialog  {
+
+	public PropertyWindow(Dialog owner) {
+		super(owner);
+		// TODO Auto-generated constructor stub
+	}
 
 }
